ceea9d3b6f601943c93761cc1f683b74a17d36ab
utaiir <utalaviation@gmail.com>
2025-11-06 15:58:49 +0300

    Update test2.lua

--- test2.lua
+++ test2.lua
@@ -1,1 +1,12 @@
-local object = "Hello"
+local AIS = {
+	"fzn53yvdutkwxg84gtgnylsxqqmb",
+	"4vafh6g5sxq1t0nr7owzp2zgn6ra",
+	"vyezo0a1kfulg5bvzulinunq1ga7",
+	"vwm4qzpt6qjt4d2vxvtvbqdexjky",
+	"8j9hptxj9s8tjj9fvsjznyes1j6d",
+	"mxjaocn7zqo3x7ssggh3067bjx5q",
+	"1jziwo9872587emzep0qbwmauwuy",
+	"mw19yvf8s2k0ywix6qp1hu8q0npe",
+	"z6k4ywbwj4bkw18a98sx962lszdk",
+}
+print("Abc")
